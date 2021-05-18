524f0ef2122ff998881974393ba4f32d165f6d3e
Jeremy Wood <js.wood206@gmail.com>
2021-05-18 11:42:24 -0700

    commit tree 4d45409205a33a813d39691843f5dadeaea2e846

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,47 @@
-let state;
+function createStore(reducer) {
+  console.log(reducer)
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  }
+
+  return { 
+    dispatch, 
+    getState
+  }
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
+    case 'RESET_COUNT':
+      return { count: 0 };
     default:
       return state;
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+let store = createStore(reducer);
+store.dispatch({ type: '@@INIT' })
 
+let button = document.getElementById('button');
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+    store.dispatch({ type: 'INCREASE_COUNT' });
+})
+
+let reset = document.getElementById('reset');
+reset.addEventListener('click', function() {
+    store.dispatch({ type: 'RESET_COUNT' });
 })
